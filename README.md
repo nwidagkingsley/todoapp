6691e0a2ca69af4713830b1ab04cd67e34af5eb1
nwidagkingsley <nkkingsley01@gmail.com>
2023-11-03 16:41:32 +0100

    this is the web app that enables you add, create, edit and delete data from a todo table

---
+++ README.md
@@ -0,0 +1,4 @@
+# TODO LIST WEB APPLICATION
+#### Video Demo:  <https://youtu.be/OF9fR5jrkNg?si=ANBwvJ_lhJ_jj3xt>
+#### Description:
+This web appliction allows you to create, read, edit and delete from a  list of table.
---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer , primary_key = True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+    
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    if request.method=='POST':
+        todo_title = request.form['title']
+        desc_todo = request.form['desc']
+        data = Todo(title=todo_title,desc=desc_todo)
+        db.session.add(data)
+        db.session.commit()
+    alltodo = Todo.query.all()
+    return render_template('index.html',alltodo=alltodo)
+
+
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        todo_title = request.form['title']
+        desc_todo = request.form['desc']
+        data = Todo.query.filter_by(sno=sno).first()
+        data.title = todo_title
+        data.desc = desc_todo
+        db.session.add(data)
+        db.session.commit()
+        return redirect("/")
+
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    return render_template('update.html',todo=todo)
+
+if __name__ == "__main__":
+    app.run(debug = True)
